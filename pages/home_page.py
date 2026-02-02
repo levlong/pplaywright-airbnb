9217697af65df2ddaeeb6a89ded36a0ef29835ae
LongLV33 <longlv33@fpt.com>
2026-02-02 13:49:28 +0700

    (feat): login test

--- pages/home_page.py
+++ pages/home_page.py
@@ -1,8 +1,4 @@
 from pages.base_page import BasePage
-from components.nav_component import NavComponent
 
 class HomePage(BasePage):
-
-    def __init__(self, page):
-        super().__init__(page)
-        self.nav = NavComponent(page)
+    pass
--- tests/test_home_page.py
+++
@@ -1,7 +0,0 @@
-from pages.home_page import HomePage
-
-def test_homepage_nav(page):
-    home = HomePage(page)
-    home.goto("https://www.airbnb.com")
-    home.nav.click_logo()
-    home.nav.click_experience_tab()
